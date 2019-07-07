0f29017ccb2224c3f98806f487d3ef0d235b926b
Paul Unsted <ed__84@hotmail.com>
2019-07-07 12:31:43 -0400

    Rows are now adding to the HTML when new trains are entered. However, for some reason I'm not passing any information to my database. Next step is to review my firebase setup to make sure that the initial data is correct and it's not a problem with my code.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-// Initialize Firebase
+$(document).ready(function () {
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDiExqaUmMa0mDauu7XFZ-Z92ivt80zwNk",
-    authDomain: "timetable-project-14470.firebaseapp.com",
-    databaseURL: "https://timetable-project-14470.firebaseio.com",
-    projectId: "timetable-project-14470",
-    storageBucket: "timetable-project-14470.appspot.com",
-    messagingSenderId: "888829465365",
-    appId: "1:888829465365:web:5fd065922103743e"
-  };
+  // Initialize Firebase
+  var firebaseConfig = {
+      apiKey: "AIzaSyDiExqaUmMa0mDauu7XFZ-Z92ivt80zwNk",
+      authDomain: "timetable-project-14470.firebaseapp.com",
+      databaseURL: "https://timetable-project-14470.firebaseio.com",
+      projectId: "timetable-project-14470",
+      storageBucket: "timetable-project-14470.appspot.com",
+      messagingSenderId: "888829465365",
+      appId: "1:888829465365:web:5fd065922103743e"
+    };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
   // Make sure to match the configuration to the script version number in the HTML
   // (Ex. 3.0 != 3.7.0)
   
-  
   // Assign the reference to the database to a variable named 'database'
   // var database = ...
   
   var database = firebase.database();
 
-    $("#add-train-btn").click(function(){
+    $("#add-train-btn").click(function(event){
 
       event.preventDefault();
 
@@ -42,14 +42,21 @@ var firebaseConfig = {
       };
 
       database.ref().push(newTra);
-
-      console.log(traNam);
-
       console.log(newTra.name);
       console.log(newTra.destination);
       console.log(newTra.time);
       console.log(newTra.next);
 
+      var newRow = $("<tr>").append(
+        $("<td>").text(traNam),
+        $("<td>").text(traDest),
+        $("<td>").text(traTime),
+        $("<td>").text(traNext)
+      );
+
+      $("#train-table > tbody").append(newRow);
+
       alert("It's a new train!");
 
     });
+});
